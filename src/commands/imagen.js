2414c93efe3c7d605a9ed3a063352f4eb3c81872
markoobradovic <marko123obradovic@gmail.com>
2024-08-01 21:18:14 +0200

    hide imagen

--- src/commands/imagen.js
+++ src/commands/imagen.js
@@ -5,10 +5,11 @@ module.exports = {
   description: 'Prompts AI to generate an image.',
   async execute(message, args) {
     try {
-      let prompt = args.join(' ');
-      const imageUrl = await useImageGen(prompt);
+      // let prompt = args.join(' ');
+      // const imageUrl = await useImageGen(prompt);
 
-      message.channel.send(`<@${message.author.id}> ${prompt} \n [open image](${imageUrl})`);
+      // message.channel.send(`<@${message.author.id}> ${prompt} \n [open image](${imageUrl})`);
+      message.channel.send('Sorry, this command is currently unavailable due to technical reasons. We are working towards the solution.');
     }
     catch (error) {
       message.channel.send(error.message);
