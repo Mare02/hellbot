3fc8f8bae59c1979aa79ceedfa13c10fedbbb6ee
markoobradovic <marko123obradovic@gmail.com>
2024-08-02 01:43:12 +0200

    remove git commit link

--- src/commands/botupdated.js
+++ src/commands/botupdated.js
@@ -6,15 +6,15 @@ module.exports = {
   name: 'botupdated',
   perm: ADMIN,
   description: 'Show a message when bot code has been updated.',
-  async execute(commitMessage, commitLink) {
+  async execute(commitMessage) {
     try {
       const client = getInstance();
       const server = await client.guilds.fetch(config.homeServerId);
       const generalChannel = await server.channels.fetch(config.generalChannelId);
 
       let messageText = `${config.bot.name}'s code has been updated!`;
-      if (commitMessage && commitMessage.length && commitLink && commitLink.length) {
-        messageText += `\n-# Update note: ["${commitMessage}"](${commitLink})`;
+      if (commitMessage && commitMessage.length) {
+        messageText += `\n-# Update note: "${commitMessage}"`;
       }
       await generalChannel.send(messageText);
     } catch (error) {
