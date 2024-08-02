97d50f4c2107f64c78319152e3595f9db2e2b218
markoobradovic <marko123obradovic@gmail.com>
2024-08-02 02:01:14 +0200

    added new mike o hearn gifs

--- src/data/gifs.js
+++ src/data/gifs.js
@@ -5,6 +5,9 @@ module.exports = {
     'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeDgyanZ1OXJncnJkaTF2aTQwZ2ViNGFkN3kxNWN6ZWhhaG54aWtrMyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/qdAzJWuIp56xE7Ei9H/giphy-downsized-large.gif',
     'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExY3Z0cjZsdDI2czY1eDBrZzlwNDJ0cHB3N2R4anloY2V2Nmptd2txciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/z7XT9h0GIGTnPC6PDP/giphy-downsized-large.gif',
     'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdm4wdW93c3h3MzluaWQyMWFydzR2enNuMTdyNzBtbmltNjdqa2I0NyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9NHhOhES33weP9LbBr/giphy-downsized-large.gif',
+    'https://media.giphy.com/media/ijsOVosEF8G9qrXX5k/giphy.gif',
+    'https://media.giphy.com/media/abxHnGDpaZicvBOiOI/giphy.gif',
+    'https://media.giphy.com/media/LaadrxjDUpnNav0RiF/giphy.gif',
   ],
   serverWelcomeGif: 'https://media1.tenor.com/m/g2TTJPBB-MAAAAAd/mikeohearn.gif',
 }
