6ffb7c97224161f3647472c886a356d72734403f
markoobradovic <marko123obradovic@gmail.com>
2024-09-04 21:36:48 +0200

    Update onNewUserJoin.js

--- src/listeners/onNewUserJoin.js
+++ src/listeners/onNewUserJoin.js
@@ -1,20 +1,16 @@
 const { getInstance } = require('../client');
-const { verifyChannelId } = require('../utils/config');
+const { verifyChannelId, homeServerId } = require('../utils/config');
 
 const client = getInstance();
 
 module.exports = () => {
   client.on('guildMemberAdd', member => {
-    const welcomeMessage = `
-      Welcome to the server, **${member.user.username}**!\nAs a new member, please take a moment to open a ticket in the <#${verifyChannelId}> channel in order for our staff to verify you.\n\n*Note: You may have to wait a little depending on the availability of our staff.*\nThanks for joining!
-    `;
+    if (member.guild.id === homeServerId) {
+      const welcomeMessage = `
+        Welcome to the server, **${member.user.username}**!\nAs a new member, please take a moment to open a ticket in the <#${verifyChannelId}> channel in order for our staff to verify you.\nThanks for joining!\n\n*Note: You may have to wait a little depending on the availability of our staff.*
+      `;
 
-    member.send(welcomeMessage)
-      .then(() => {
-        console.log(`DM sent to ${member.user.tag}`);
-      })
-      .catch(error => {
-        console.error(`Error sending DM to ${member.user.tag}: ${error}`);
-      });
+      member.send(welcomeMessage);
+    }
   });
 };
