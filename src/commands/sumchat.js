030156947290f0853bd5a16aba5853c578682b6d
markoobradovic <marko123obradovic@gmail.com>
2024-09-10 10:42:51 +0200

    sumchat using slash

--- src/commands/sumchat.js
+++ src/commands/sumchat.js
@@ -4,24 +4,51 @@ const { emptyState } = require('../utils/messages');
 module.exports = {
   name: 'sumchat',
   description: 'Summarizes the recent chat.',
-  async execute(message) {
+  slash: true,
+  async execute(interaction, args) {
     try {
-      const channel = message.channel;
+      if (!args) {
+        await interaction.deferReply();
+      }
+
+      const channel = interaction.channel;
       const messages = await channel.messages.fetch({ limit: 20 });
+
+      let username;
+      if (!args) {
+        username = interaction.user.username;
+      } else {
+        username = interaction.author.username;
+      }
       const messagesArray = messages.map(message => ({
         content: message.content,
-        author: message.author.username,
+        author: username,
       }));
+
       if (!messagesArray.length) {
-        await message.channel.send(emptyState.noResponseSummarize);
+        if (!args) {
+          interaction.editReply(emptyState.noResponseSummarize);
+        } else {
+          interaction.channel.send(emptyState.noResponseSummarize);
+        }
         return;
       }
+
       const messagesContentString = messagesArray.reverse().map(msg => `${msg.author}: ${msg.content}`).join('<br>');
       const summary = await usePrompt(`Please summarize the following conversation: \n${messagesContentString}`);
-      message.channel.send(summary);
+
+      if (!args) {
+        interaction.editReply(summary);
+      } else {
+        interaction.channel.send(summary);
+      }
     } catch (error) {
       console.error(error);
-      message.channel.send(error.message);
+      if (!args) {
+        interaction.editReply(error.message);
+      } else {
+        interaction.channel.send(error.message);
+      }
     }
   },
 };
