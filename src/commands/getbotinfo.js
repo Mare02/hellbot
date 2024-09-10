02f3152b13a53ce6b74b6652991896759ed2a2cf
markoobradovic <marko123obradovic@gmail.com>
2024-09-10 08:36:35 +0200

    getbotinfo using slash

--- src/commands/getbotinfo.js
+++ src/commands/getbotinfo.js
@@ -3,16 +3,22 @@ const { ADMIN } = require('../utils/roles');
 const config = require('../utils/config');
 
 module.exports = {
-  name: 'getinfo',
+  name: 'getbotinfo',
   description: 'Get technical bot information.',
-  async execute(message, args) {
-    message.channel.send(`
+  slash: true,
+  async execute(interaction, args) {
+    const info = `
 **Name:** ${config.bot.name} (${config.bot.version}) (${config.bot.decription})
 **Owner:** ${config.owner.name} (${config.owner.id})
 **Environment:** ${process.env.NODE_ENV}
 **Commands prefix:** ${config.commandsPrefix}
 **Using AI model:** ${config.currentAiModel}
 **Host sytem:** ${process.platform} ${process.arch}
-    `);
+    `
+    if(!args){
+      interaction.reply(info);
+    } else {
+      interaction.channel.send(info);
+    }
   },
 };
