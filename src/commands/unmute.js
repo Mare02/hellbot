594ab045be524db0a1fde84903912f3af62031b0
markoobradovic <marko123obradovic@gmail.com>
2024-09-12 01:24:04 +0200

    unmute user

--- src/commands/unmute.js
+++ src/commands/unmute.js
@@ -1,20 +1,15 @@
 const messages = require('../utils/messages');
 
 module.exports = {
-  name: 'mute',
-  description: 'Mutes a user in the server.',
+  name: 'unmute',
+  description: 'Unmutes a user in the server.',
   slash: true,
   params: [
     {
       name: 'user',
-      description: 'The user to mute',
+      description: 'The user to unmute',
       type: 6,
       required: true,
-    },
-    {
-      name: 'duration',
-      description: 'Duration of the mute in milliseconds (ms)',
-      required: false,
     },
   ],
   async execute(interaction, args) {
@@ -31,36 +26,25 @@ module.exports = {
         return await reply(messages.system.noPermission);
       }
 
-      let userToMute;
+      let userToUnmute;
       if (!args) {
-        userToMute = await interaction.guild.members.fetch(
+        userToUnmute = await interaction.guild.members.fetch(
           interaction.options.getUser('user')
         ).catch(() => null);
       } else if (interaction.mentions.users.size) {
-        userToMute = await interaction.guild.members.fetch(
+        userToUnmute = await interaction.guild.members.fetch(
           interaction.mentions.users.first().id
         ).catch(() => null);
       } else if (!interaction.mentions.users.size && args.length) {
-        userToMute = await interaction.guild.members.fetch(args[0]).catch(() => null);
+        userToUnmute = await interaction.guild.members.fetch(args[0]).catch(() => null);
       }
 
-      if (!userToMute || Array.isArray(userToMute)) {
+      if (!userToUnmute || Array.isArray(userToUnmute)) {
         return await reply('User not found. Please provide a valid user ID or mention.');
       }
 
-      let duration;
-      if (!args && interaction.options.getString('duration')) {
-        duration = parseInt(interaction.options.getString('duration'));
-      } else if (args && args[1] && args[1].length) {
-        duration = parseInt(args[1]);
-      }
-
-      await userToMute.timeout(duration);
-      await reply(`**${userToMute.displayName}** has been muted${
-        duration
-          ? ` for ${duration / 1000} ${duration / 1000 > 1 ? 'seconds' : 'second'}`
-          : ''
-      }.`);
+      await userToUnmute.timeout(null);
+      await reply(`**${userToUnmute.displayName}** has been unmuted.`);
     }
     catch (error) {
       console.error(error.message);
