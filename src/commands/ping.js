20c7a54654746433edb8ed64554d53e543f10aca
markoobradovic <marko123obradovic@gmail.com>
2025-01-24 00:15:31 +0100

    fix?

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,15 +4,23 @@ module.exports = {
   name: 'ping',
   description: 'Pings and tells the latency.',
   slash: true,
-  async execute(interaction) {
+  async execute(interaction, args) {
     try {
+      if (!args) {
+        await interaction.deferReply();
+      }
+
       const sent = await reply(interaction, null, 'Pinging...');
       const latency = sent.createdTimestamp - interaction.createdTimestamp;
 
       await sent.edit(`Pong! Latency is ${latency}ms.`);
     } catch (error) {
       console.error(error);
-      await reply(interaction, null, 'An error occurred while pinging.').catch(console.error);
+      if (!args) {
+        await interaction.editReply(error.message);
+      } else {
+        await interaction.reply(error.message);
+      }
     }
   },
 };
