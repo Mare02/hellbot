fa86a00bab121534a5c4060982ad2e11c948070c
markoobradovic <marko123obradovic@gmail.com>
2025-01-24 00:56:32 +0100

    test

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,16 +2,30 @@ module.exports = {
   name: 'ping',
   description: 'Pings and tells the latency.',
   slash: true,
-  async execute(interaction) {
-    try {
-      const start = Date.now();
+  async execute(interaction, args) {
+    const start = Date.now();
 
-      const pong = await interaction.channel.send('Pinging...');
+    try {
+      if (!args) {
+        interaction.deferReply();
+      }
+
+      if (!args) {
+        await interaction.editReply('Pinging...');
+      }
+      else {
+        await interaction.reply('Pinging...');
+      }
 
       const end = Date.now();
       const latency = end - start;
 
-      await pong.edit(`Pong! Latency is ${latency}ms.`);
+      if (!args) {
+        await interaction.editReply(`Pong! Latency is ${latency}ms.`);
+      }
+      else {
+        await interaction.reply(`Pong! Latency is ${latency}ms.`);
+      }
     }
     catch (error) {
       console.error(error);
