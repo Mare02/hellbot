2d35e21cf270f494498d7ddc47715fe571711351
markoobradovic <marko123obradovic@gmail.com>
2025-06-29 23:45:47 +0200

    fix verify command input type

--- src/commands/verify.js
+++ src/commands/verify.js
@@ -46,7 +46,7 @@ module.exports = {
         return await reply(interaction, args, 'Please provide age and invite source.');
       }
 
-      if (typeof Number(age) !== 'number' || typeof inviteSource !== 'string') {
+      if (isNaN(Number(age)) || !isNaN(Number(inviteSource))) {
         return await reply(interaction, args, 'Invalid age or invite source.');
       }
 
